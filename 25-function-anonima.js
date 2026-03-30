21787cdc94168ca5462699593a3b33701d1d77a0
tiowill-dev <william_mw@outlook.com>
2026-03-30 20:44:50 -0300

    aula js

---
+++ 25-function-anonima.js
@@ -0,0 +1,12 @@
+/* Function Anonima */ 
+
+const sum = function(number1, number2) {
+    return number1 + number2;
+};
+const firstNumber = 5
+const secondNumber = 10
+ console.log('o primeiro número é', firstNumber)
+console.log('o segundo número é', secondNumber)
+const sumResult = sum(firstNumber, secondNumber)
+
+console.log('a soma é', sumResult)
