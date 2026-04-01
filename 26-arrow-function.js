cb48614dfdecfb5935de9e6faa469021ae83d4fc
tiowill-dev <william_mw@outlook.com>
2026-04-01 16:32:35 -0300

    aula 26 - Arrow Function

---
+++ 26-arrow-function.js
@@ -0,0 +1,11 @@
+/*Arrow function é uma forma abteviada de escrever em JavaScript.*/
+
+
+const sum = (number1, number2) => number1 + number2 
+
+const firstNumber = 5
+const secondNumber = 10
+console.log(`o primeiro número é ${ firstNumber }`)
+console.log(`o segundo número é ${ secondNumber }`)
+
+console.log(`a soma é ${ sum(firstNumber, secondNumber) }`)
